b60153433a625b34afc1298dab566ef86ad99327
dscrane <daegancrane@gmail.com>
2021-02-07 10:42:59 -0500

    added bootstrap styles

--- jotto/src/App.js
+++ jotto/src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
+import GuessedWords from "./components/guessedWords/GuessedWords";
+import Congrats from "./components/congrats/Congrats";
 import './App.css';
 
 function App() {
   return (
-    <div>
-
+    <div className='container'>
+      <h1>Jotto</h1>
+        <Congrats success={ true } />
+        <GuessedWords guessedWords={[
+          { guessedWord: 'train', letterMatchCount: 3}
+        ]} />
     </div>
   );
 }
