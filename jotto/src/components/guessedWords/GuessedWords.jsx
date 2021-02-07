4c2243379ffff2eabc8b0bd5fc2800dff45adbc8
dscrane <daegancrane@gmail.com>
2021-02-07 10:32:31 -0500

    tests and code for non zero guessed words

--- jotto/src/components/guessedWords/GuessedWords.jsx
+++ jotto/src/components/guessedWords/GuessedWords.jsx
@@ -9,8 +9,29 @@ const GuessedWords = (props) => {
         Try to guess the secret word!
       </span>
     )
+  } else {
+    const guessedWordRows = props.guessedWords.map((word, i) => {
+      return (
+        <tr data-test='guessed-word' key={i}>
+          <td>{word.guessedWord}</td>
+          <td>{word.letterMatchCount}</td>
+        </tr>
+      )
+    });
+    contents = (
+      <div data-test='guessed-words'>
+        <h3>Guessed Words</h3>
+        <table>
+          <thead>
+            <tr><th>Guess</th><th>Matching Letters</th></tr>
+          </thead>
+          <tbody>
+            { guessedWordRows }
+          </tbody>
+        </table>
+      </div>
+    )
   }
-  console.log(contents)
   return (
     <div data-test='component-guessed-words'>
       {contents}
