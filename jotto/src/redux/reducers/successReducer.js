edad39d51cdc260ed7ee513610b26e1f6dd0883c
dscrane <daegancrane@gmail.com>
2021-02-08 08:51:07 -0500

    coded successReducer and successReduer.test

--- jotto/src/redux/reducers/successReducer.js
+++ jotto/src/redux/reducers/successReducer.js
@@ -4,7 +4,13 @@
  * @param {object} action -- action to be reduced
  * @returns {boolean} -- new success state
  */
+import {actionTypes} from "../actions";
 
-export default (state, action) => {
-  return null;
+export default (state=false, action) => {
+  switch(action.type) {
+    case (actionTypes.CORRECT_GUESS):
+      return true;
+    default:
+      return state
+  }
 }
